3101030e6fa114c6072fb84a3038fb46d90fcaee
Maxim <maxim.oweyssi@est.org.uk>
2024-08-13 18:13:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,38 @@ ji_columns = ['Activity group (JI)',
 
 # Columns for plotting
 plot_columns = [
-    'Product complexity index', 'Market concentration (HHI)',
-    'Current RCA', '2022 Trade Value', '2022 Trade Value Relatedness', '2022 Trade Value RCA', '2022 HHI',
-    '2022 Market share', 'PCI Rank', 'PCI', 'TradeValueCAGR', 'Market Value CAGR', 'Market Share CAGR',
-    'HHI CAGR', 'CAGR Start Year'
+    'Product complexity index',
+    'Market concentration (HHI)',
+    'Current RCA',
+    '2022 Trade Value',
+    '2022 Trade Value Relatedness',
+    '2022 Trade Value RCA',
+    '2022 HHI',
+    '2022 Market share',
+    'PCI Rank',
+    'PCI',
+    'TradeValueCAGR',
+    'Market Value CAGR', 
+    'Market Share CAGR',
+    'HHI CAGR', 
 ]
+hover_data = ['Product Number', 
+              'HS6', 
+              'HS4', 
+              'HS2', 
+              'Country', 
+              'Environmental benefit', 
+              'Green ex-out',
+              'CAGR Start Year', 
+              'CAGR End Year']
 
 x_axis = st.sidebar.selectbox("Select X-axis variable", plot_columns)
 y_axis = st.sidebar.selectbox("Select Y-axis variable", plot_columns)
 color = st.sidebar.selectbox("Select color variable", ji_columns)
-hover_data = ['Product Number', 'HS6', 'HS4', 'HS2', 'Country', 'Environmental benefit', 'Green ex-out' ]
-
+hover_info = st.sidebar.multiselect("Select what info should appear on hover",hover_data)
 # Plotting
 st.header(f"Scatter Plot of {x_axis} vs {y_axis}")
 
-fig = px.scatter(df, x=x_axis, y=y_axis, color=color, title=f'{x_axis} vs {y_axis} colored by {color}', hover_data=hover_data)
+fig = px.scatter(df, x=x_axis, y=y_axis, color=color, title=f'{x_axis} vs {y_axis} colored by {color}', hover_data=hover_info)
+
 st.plotly_chart(fig)
