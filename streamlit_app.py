4bada2e2d8869768ccd7f76a999ea463c1e28092
Maxim <maxim.oweyssi@est.org.uk>
2024-08-19 21:06:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,7 @@ plot_columns = [
 'EU_2030_export',
 'EU_Total_Export_25_30',
 'CAGR_2022_30_FORECAST',
-'stejna velikost',
-'IS_REALCAGR'
+'stejna velikost'
 ]
 hover_data = ['HS_ID', 
               'Produkt_HS6',
@@ -53,7 +52,8 @@ hover_data = ['HS_ID',
               'Produkt_HS2',
               'EU_Total_Export_25_30',
               'CZ_Total_Export_25_30',
-              'Zdroj'
+              'Zdroj',
+              'IS_REALCAGR'
               ]
 
 x_axis = st.sidebar.selectbox("Select X-axis variable", plot_columns,index=0)
