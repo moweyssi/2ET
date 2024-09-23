e562ea7f0a69beaf1d78afeaebcd553810f89f5b
Maxim <maxim.oweyssi@est.org.uk>
2024-09-23 09:48:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -270,7 +270,7 @@ if HS_select == []:
                      #height='100%',
                      opacity=0.7,
                      size=markersize,
-                     size_max=40, equal_axes=True)
+                     size_max=40)
     
 
 else:
@@ -285,7 +285,7 @@ else:
                      #height=700,
                      opacity=0.7,
                      size=markersize,
-                     size_max=40, equal_axes=True
+                     size_max=40
                      )
 
 fig.update_layout(
