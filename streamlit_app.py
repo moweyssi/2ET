a223f0c20b3f8638f60fa365a04dbab28e8f941d
Maxim <maxim.oweyssi@est.org.uk>
2024-09-27 13:23:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from io import StringIO
 import plotly.io as pio
 #st.set_page_config(layout="wide")
 st.title("Mapa Příležitostí")
-st.markdown('<i style="color:red;">Toto je pracovní verze. Projekce 2025-30 prosíme brát s velikou rezervou</i>', unsafe_allow_html=True)
+st.markdown('<i style="color:red;">Toto je pracovní verze. Data krom budoucího růstu jsou z oec.world. Projekce 2025-30 berte s velikou rezervou.</i>', unsafe_allow_html=True)
 # Sidebar for selecting variables
 st.sidebar.header("Nastavení Grafu")
 
