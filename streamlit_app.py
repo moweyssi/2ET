d5ca7a054f5d9dabb5b5593992a3b9b1df489fc9
Maxim <maxim.oweyssi@est.org.uk>
2024-10-01 11:48:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -320,9 +320,15 @@ fig.update_layout(
 st.plotly_chart(fig)
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Vybraný český export za rok 2022", "{:,.0f}".format(sum(filtered_df['CZ_export_2022'])/1000000000),'miliard CZK' )
-col2.metric("Vybraný český export 2025 až 2030", "{:,.0f}".format(sum(filtered_df['CZ_Total_Export_25_30'])/1000000000), "miliard CZK")
-col3.metric("Vybraný evropský export 2025 až 2030", "{:,.0f}".format(sum(filtered_df['EU_Total_Export_25_30'])/1000000000), "miliard CZK")
+
+if HS_select == []:
+    col1.metric("Vybraný český export za rok 2022", "{:,.0f}".format(sum(filtered_df['CZ_export_2022'])/1000000000),'miliard CZK' )
+    col2.metric("Vybraný český export 2025 až 2030", "{:,.0f}".format(sum(filtered_df['CZ_Total_Export_25_30'])/1000000000), "miliard CZK")
+    col3.metric("Vybraný evropský export 2025 až 2030", "{:,.0f}".format(sum(filtered_df['EU_Total_Export_25_30'])/1000000000), "miliard CZK")
+else:
+    col1.metric("Vybraný český export za rok 2022", "{:,.0f}".format(sum(filtered_df[filtered_df['Název Produktu'].isin(HS_select)]['CZ_export_2022'])/1000000000),'miliard CZK' )
+    col2.metric("Vybraný český export 2025 až 2030", "{:,.0f}".format(sum(filtered_df[filtered_df['Název Produktu'].isin(HS_select)]['CZ_Total_Export_25_30'])/1000000000), "miliard CZK")
+    col3.metric("Vybraný evropský export 2025 až 2030", "{:,.0f}".format(sum(filtered_df[filtered_df['Název Produktu'].isin(HS_select)]['EU_Total_Export_25_30'])/1000000000), "miliard CZK")
 
 
 mybuff = StringIO()
